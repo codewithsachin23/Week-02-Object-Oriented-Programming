e919316da0e972f383bdff32bf198dfe2bd85db2
sachin shukla <sachinshukla232003@gmail.com>
2025-01-18 22:19:34 +0530

    Added Book details day 2 level 1

---
+++ src/Day_02_Level_1/Day_02_Java_Constructor/BookDetails.java
@@ -0,0 +1,44 @@
+package Day_02_Level_1.Day_02_Java_Constructor;
+class Book{
+    String title; 
+    String author; 
+    double price;
+
+    // Default constructor
+    public Book() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+    }
+
+
+    //parameterized constructer 
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+     // Method to display book details
+     public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+        System.out.println();
+    }
+
+    
+    
+}
+public class BookDetails{
+    public static void main(String[] args) {
+        //making default book to reference nothing
+        Book defaultBook = new Book(); 
+        defaultBook.displayDetails(); 
+        //book 1 to store data
+        Book book1 = new Book("programming using java", "R. N. Sons", 1122);
+        
+        //display the book details
+        book1.displayDetails();
+    }
+}
