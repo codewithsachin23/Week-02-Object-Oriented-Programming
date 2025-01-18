7877e859e94e90882276369a2f6f94db2de5291f
sachin shukla <sachinshukla232003@gmail.com>
2025-01-18 22:33:46 +0530

    Added Employee day 2 level 1

---
+++ src/Day_02_Level_1/Day_02_Java_Access_Modifiers/EmployeeRecords.java
@@ -0,0 +1,43 @@
+package Day_02_Level_1.Day_02_Java_Access_Modifiers;
+class Employee {
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    // Method to modify salary
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Method to get salary
+    public double getSalary() {
+        return salary;
+    }
+}
+
+class Manager extends Employee {
+    // Method to display details of the manager
+    public void displayDetails() {
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+    }
+}
+
+public class EmployeeRecords {
+    public static void main(String[] args) {
+        // Creating an object of Employee
+        Employee employee = new Employee();
+        employee.employeeID = 101;
+        employee.department = "HR";
+        employee.setSalary(50000.0);
+        System.out.println("Employee ID: " + employee.employeeID);
+        System.out.println("Department: " + employee.department);
+        System.out.println("Salary: " + employee.getSalary());
+
+        // Creating an object of Manager
+        Manager manager = new Manager();
+        manager.employeeID = 102;
+        manager.department = "Finance";
+        manager.displayDetails();
+    }
+}
